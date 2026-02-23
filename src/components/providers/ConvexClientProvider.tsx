ac23dd333c2580239e9d41d8b97e97697f496672
asm-00 <mukombambia@gmail.com>
2026-02-23 16:13:20 +0200

    vercel

--- src/components/providers/ConvexClientProvider.tsx
+++ src/components/providers/ConvexClientProvider.tsx
@@ -5,7 +5,7 @@ import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ReactNode } from "react";
 import { UserProvider } from "./UserProvider";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "https://admired-falcon-221.convex.cloud");
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "https://optimistic-starling-894.convex.cloud");
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
     return (
